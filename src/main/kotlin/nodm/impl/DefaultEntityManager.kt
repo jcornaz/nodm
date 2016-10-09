f9072bc0eb6d171678fc3c7cd11c324f0ebbdfc1
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2016-10-09 14:36:44 +0200

    Remove guava

--- src/main/kotlin/nodm/impl/DefaultEntityManager.kt
+++ src/main/kotlin/nodm/impl/DefaultEntityManager.kt
@@ -1,6 +1,6 @@
 package nodm.impl
 
-import com.google.common.collect.HashBasedTable
+import kable.HashTable
 import nodm.EntityManager
 import nodm.UniversalID
 import java.util.*
@@ -8,7 +8,7 @@ import kotlin.reflect.KClass
 
 class DefaultEntityManager : EntityManager {
 
-    private val entities = HashBasedTable.create<UniversalID, KClass<*>, Any>()!!
+    private val entities = HashTable<UniversalID, KClass<*>, Any>()
     private val unids = HashMap<Any, UniversalID>()
 
     override fun get(universalID: UniversalID, klass: KClass<*>): Any? =
@@ -21,7 +21,7 @@ class DefaultEntityManager : EntityManager {
     }
 
     override fun contains(entity: Any): Boolean = synchronized(this) {
-        entities.containsValue(entity)
+        entity in entities
     }
 
     override fun unidOf(entity: Any): UniversalID? = unids[entity]
